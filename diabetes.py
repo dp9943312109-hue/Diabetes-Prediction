1c505df87136691a9c2e22cef0865df32c2d9974
dp9943312109-hue <dp9943312109@gmail.com>
2026-05-21 12:06:16 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model selection import train test split
-from sklearn.linear model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 #Load the dataset
 data pd.read_csv('diabetes.csv')
